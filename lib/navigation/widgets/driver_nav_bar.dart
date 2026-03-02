c83600bcc20ad58f6670bc93566aceef2f0014ef
Akero <akeroshii@gmail.com>
2026-03-02 09:29:52 +0800

    driver nav bar revise

--- lib/navigation/widgets/driver_nav_bar.dart
+++ lib/navigation/widgets/driver_nav_bar.dart
@@ -14,43 +14,70 @@ class DriverNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
-      ),
-      child: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        onTap: onTap,
-        backgroundColor: Colors.white,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: AppColors.primaryBlue,
-        unselectedItemColor: AppColors.darkNavy.withOpacity(0.3),
-        selectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.w900,
-          fontSize: 10,
-        ),
-        unselectedLabelStyle: const TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 10,
-        ),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.dashboard_rounded),
-            label: AppTexts.navHome,
+    return BottomAppBar(
+      height: 75,
+      color: Colors.white,
+      elevation: 10,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _navItem(Icons.home_filled, AppTexts.navHome, 0), // Same as Passenger
+          _navItem(
+            Icons.account_balance_wallet_rounded,
+            AppTexts.navEarnings,
+            1,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_balance_wallet_rounded),
-            label: AppTexts.navEarnings,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.minor_crash_rounded),
-            label: AppTexts.navVehicle,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_rounded),
-            label: AppTexts.navProfile,
-          ),
+          _navItem(Icons.minor_crash_rounded, AppTexts.navVehicle, 2),
+          _navItem(
+            Icons.person_outline,
+            AppTexts.navProfile,
+            3,
+          ), // Same as Passenger
         ],
+      ),
+    );
+  }
+
+  Widget _navItem(IconData icon, String label, int index) {
+    bool isSelected = selectedIndex == index;
+    return InkWell(
+      onTap: () => onTap(index),
+      child: SizedBox(
+        width: 60,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              color: isSelected
+                  ? AppColors.primaryBlue
+                  : AppColors.darkNavy.withOpacity(0.3),
+              size: 24,
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: isSelected ? FontWeight.w900 : FontWeight.w600,
+                color: isSelected
+                    ? AppColors.primaryBlue
+                    : AppColors.darkNavy.withOpacity(0.3),
+              ),
+            ),
+            if (isSelected)
+              Container(
+                margin: const EdgeInsets.only(top: 1),
+                height: 2,
+                width: 12,
+                decoration: BoxDecoration(
+                  color: AppColors.primaryYellow,
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              )
+            else
+              const SizedBox(height: 3),
+          ],
+        ),
       ),
     );
   }
